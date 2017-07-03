83085ad199e6dffe5443df1ddc54fe0eb82d98cc
Harm Hofstee <harm@tallcomponents.com>
2017-07-03 23:03:04 +0200

    ImageDiffWpf app

---
+++ ImageDiffWpf/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GalaSoft.MvvmLight;
+
+namespace Imagediff
+{
+    public class Imagediffs : ObservableCollection<ImagediffViewModel> { }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+  //      Imagediffs names;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //this.toggleButton.Click += toggleButton_Click;
+
+   //         this.names = new Imagediffs();
+
+            //dockPanel.DataContext = this.names;
+        }
+
+        //void toggleButton_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+    }
+}
---
+++ ImageDiffWpf/ViewModels/ImageDiffViewModel.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using System.Windows.Input;
+
+namespace Imagediff
+{
+    public class ImagediffViewModel : ViewModelBase
+    {
+        private string image_name;
+        private string adobe_image_name;
+        private string diff_image_name;
+        private string ref_image_name;
+        private string left_image_name;
+        private string right_image_name;
+        private string left_toggle_button_name;
+        private string right_toggle_button_name;
+
+        //protected void OnPropertyChanged(string propName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        //}
+
+        public ImagediffViewModel() : this( "Image Name", "Adobe Image Name", "Diff Image Name", "Ref Image Name") { }
+        public ImagediffViewModel(string image_name, string adobe_image_name, string diff_image_name, string ref_image_name)
+        {
+            ImageName = image_name;
+            AdobeImageName = adobe_image_name;
+            DiffImageName = diff_image_name;
+            RefImageName = ref_image_name;
+            LeftImageName = image_name;
+            RightImageName = ref_image_name;
+            LeftToggleButtonName = "Image";
+            RightToggleButtonName = "Reference";
+        }
+
+        public string ImageName
+        {
+            get
+            {
+                return image_name;
+            }
+            set
+            {
+                image_name = value;
+                RaisePropertyChanged("ImageName");
+            }
+        }
+
+        public string AdobeImageName
+        {
+            get
+            {
+                return adobe_image_name;
+            }
+            set
+            {
+                adobe_image_name = value;
+                RaisePropertyChanged("AdobeImageName");
+            }
+        }
+
+        public string DiffImageName
+        {
+            get
+            {
+                return diff_image_name;
+            }
+            set
+            {
+                diff_image_name = value;
+                RaisePropertyChanged("DiffImageName");
+            }
+        }
+
+        public string RefImageName
+        {
+            get
+            {
+                return ref_image_name;
+            }
+            set
+            {
+                ref_image_name = value;
+                RaisePropertyChanged("RefImageName");
+            }
+        }
+
+        public string LeftImageName
+        {
+            get
+            {
+                return left_image_name;
+            }
+            set
+            {
+                left_image_name = value;
+                RaisePropertyChanged("LeftImageName");
+            }
+        }
+
+        public string RightImageName
+        {
+            get
+            {
+                return right_image_name;
+            }
+            set
+            {
+                right_image_name = value;
+                RaisePropertyChanged("RightImageName");
+            }
+        }
+
+        public string LeftToggleButtonName
+        {
+            get
+            {
+                return left_toggle_button_name;
+            }
+            set
+            {
+                left_toggle_button_name = value;
+                RaisePropertyChanged("LeftToggleButtonName");
+            }
+        }
+
+        public string RightToggleButtonName
+        {
+            get
+            {
+                return right_toggle_button_name;
+            }
+            set
+            {
+                right_toggle_button_name = value;
+                RaisePropertyChanged("RightToggleButtonName");
+            }
+        }
+
+        public ICommand ToggleLeft
+        {
+            get
+            {
+                return new ToggleCommand(this, "Left", new string[] { "Image", "Reference" });
+            }
+        }
+
+        public ICommand ToggleRight
+        {
+            get
+            {
+                return new ToggleCommand(this, "Right", new string[] {"Reference", "Diff", "Adobe"});
+            }
+        }
+    }
+
+    public class ToggleCommand : ICommand
+    {
+        ImagediffViewModel imagediff;
+        string left_or_right;
+        string[] button_names;
+
+        public ToggleCommand(ImagediffViewModel imagediff, string left_or_right, string[] button_names)
+        {
+            this.imagediff = imagediff;
+            this.left_or_right = left_or_right;
+            this.button_names = button_names;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            string current_button_name = left_or_right == "Left" ? imagediff.LeftToggleButtonName : imagediff.RightToggleButtonName;
+            int i = (Array.IndexOf(button_names, current_button_name) + 1) % button_names.Length;
+            string image_name = "";
+
+            switch (button_names[i])
+            {
+                case "Image":
+                    image_name = imagediff.ImageName;
+                    break;
+                case "Adobe":
+                    image_name = imagediff.AdobeImageName;
+                    break;
+                case "Diff":
+                    image_name = imagediff.DiffImageName;
+                    break;
+                case "Reference":
+                    image_name = imagediff.RefImageName;
+                    break;
+            }
+
+            if (left_or_right == "Left")
+            {
+                imagediff.LeftToggleButtonName = button_names[i];
+                imagediff.LeftImageName = image_name;
+            }
+            else
+            {
+                imagediff.RightToggleButtonName = button_names[i];
+                imagediff.RightImageName = image_name;
+            }
+        }
+    }
+}
